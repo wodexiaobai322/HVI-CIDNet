71924b9566e62450eee28be76537a07ab60e65f3
wodexiaobai322 <wodepigen@yeah.net>
2025-03-30 17:59:04 +0800

    .

--- data/options.py
+++ data/options.py
@@ -3,9 +3,9 @@ import argparse
 def option():
     # Training settings
     parser = argparse.ArgumentParser(description='CIDNet')
-    parser.add_argument('--batchSize', type=int, default=8, help='training batch size')
+    parser.add_argument('--batchSize', type=int, default=4, help='training batch size')
     parser.add_argument('--cropSize', type=int, default=256, help='image crop size (patch size)')
-    parser.add_argument('--nEpochs', type=int, default=1000, help='number of epochs to train for end')
+    parser.add_argument('--nEpochs', type=int, default=500, help='number of epochs to train for end')
     parser.add_argument('--start_epoch', type=int, default=0, help='number of epochs to start, >0 is retrained a pre-trained pth')
     parser.add_argument('--snapshots', type=int, default=10, help='Snapshots for save checkpoints pth')
     parser.add_argument('--lr', type=float, default=1e-4, help='Learning Rate')
